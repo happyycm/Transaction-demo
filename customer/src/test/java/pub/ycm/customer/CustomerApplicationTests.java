8b23e048f6669f4ea0beb9b9d832123307fedd93
杨长明 <happyycm@gmail.com>
2020-03-12 10:25:15 +0800

    Initial commit

---
+++ customer/src/test/java/pub/ycm/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package pub.ycm.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
